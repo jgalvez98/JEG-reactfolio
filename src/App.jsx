7af4b8ab917a8032bb526a1e1a1ea7af98dfc986
juanes G <jgalvez98@gmail.com>
2023-11-07 20:56:16 -0500

    added navbar.jsx, header.jsx and social.jsx and the profile carpet containing .json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
-import PortfolioContainer from "./components/PortfolioContainer";
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import useDarkMode from 'use-dark-mode';
+import AppContext from './AppContext';
+import MainApp from './MainApp';
+import GlobalStyles from './theme/GlobalStyles';
+import { lightTheme, darkTheme } from './theme/themes';
 
-const App = () => <PortfolioContainer />;
+function App() {
+  window.matchMedia = null;
+  const darkMode = useDarkMode(true);
+
+  return (
+    <AppContext.Provider value={{ darkMode }}>
+      <ThemeProvider theme={darkMode.value ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <div className="App">
+          <BrowserRouter>
+            <MainApp />
+          </BrowserRouter>
+        </div>
+      </ThemeProvider>
+    </AppContext.Provider>
+  );
+}
 
 export default App;
