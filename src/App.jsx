87cb0d034b94f0e0ff1f30c0048385f05d16fd95
juanes G <jgalvez98@gmail.com>
2023-11-21 17:20:22 -0500

    main.jsx changed rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar.jsx";
-// import HeroContainer from "./components/UI/HeroContainer";
-// import AboutMeContainer from "./components/UI/AboutMeContainer";
-// import MySkillsContainer from "./components/UI/MySkillsContainer.jsx";
-// import PortfolioContainer from "./components/UI/PortfolioContainer.jsx";
-//import ContactMeContainer from "./components/UI/contactMeContainer.jsx";
+import PortfolioContainer from "./components/UI/PortfolioContainer.jsx";
+import ContactMeContainer from "./components/UI/contactMeContainer.jsx";
 import Footer from "./components/UI/Footer/Footer.jsx";
 import { Outlet } from "react-router-dom";
 import $ from "jquery";
--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import AboutMeContainer from './components/UI/AboutMeContainer';
-import PortfolioContainer from './components/UI/PortfolioContainer';
-import ContactMeContainer from './components/UI/ContactMeContainer';
-import ResumeContainer from './components/UI/ResumeContainer' ;
 
-const router = createBrowserRouter([
-    {
-      path: "#",
-      element: <App />,
-      children: [
-       {
-          path: '#aboutme',
-          element: <AboutMeContainer/>
-        }, {
-          path: '#portfolio',
-          element: <PortfolioContainer />
-        }, {
-          path: '#contact',
-          element: <ContactMeContainer />
-        },
-        {
-          path: '#resume',
-          element: <ResumeContainer />
-        }
-      ]
-    },
-  ]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router}/>);
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
