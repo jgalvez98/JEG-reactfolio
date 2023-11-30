bc440a264d7d92ce67a5eae0daddbcbc63cf61bb
juanes G <jgalvez98@gmail.com>
2023-11-29 19:14:09 -0500

    fixed some style and fixed row on portfolio not displaying elemnts correctly

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,7 +9,7 @@ export default function Cards() {
           <h1>My Projects</h1>
           <h1>My Projects</h1>
         </div>
-			<div className="row justify-content-center" >
+			<div className="row d-flex justify-content-center" >
 
 				<div className="card">
 					<img src="./public/weatherapp.png" className="card-img" alt="..." />
@@ -17,6 +17,8 @@ export default function Cards() {
 						<p className="cardText">Check a 5 day forecast in any city</p>
 						<div className="row justify-content-center">
 							<a href="https://jgalvez98.github.io/jeg-ProWeather/" className="cardBtn">Website</a>
+							<a href="https://github.com/jgalvez98/jeg-ProWeather" className="cardBtn2">Github</a>
+
 						</div>
 					</div>
 				</div>
@@ -27,41 +29,41 @@ export default function Cards() {
 						<p className="cardText">Recipe finder app</p>
 						<div className="row justify-content-center">
 							<a href="https://dashboard.heroku.com/apps/project-2-recipeasy" className="cardBtn">Website</a>
+							<a href="https://github.com/arceojoshua1994/full-stack-project-2" className="cardBtn2">Github</a>
 						</div>
 					</div>
 				</div>
 
 				<div className="card">
-					<img src="./public/cruisecontrol.png" className="card-img" alt="note taker" />
+					<img src="./public/cruisecontrol.png" className="card-img" alt="cruise planner" />
 					<div className="card-body">
 						<p className="cardText">A cruise planner app</p>
 						<div className="row justify-content-center">
 							<a href="https://ivega16.github.io/cruisecontrol/" className="cardBtn">Website</a>
+							<a href="https://github.com/ivega16/cruisecontrol" className="cardBtn2">Github</a>
 						</div>
 					</div>
 				</div>
 
-			</div>
-
-			<div className="row justify-content-center">
-
 				<div className="card">
-					<img src="./public/texteditor.png" className="card-img" alt="..." />
+					<img src="./public/texteditor.png" className="card-img" alt="text editor" />
 					<div className="card-body">
 						<p className="cardText">create notes online or offline</p>
 						<div className="row justify-content-center">
 							<a href="https://jeg-texteditor-1f0435e08f5b.herokuapp.com/" className="cardBtn">Website</a>
+							<a href="https://github.com/jgalvez98/JEG-TextEditor" className="cardBtn2">Github</a>
 						</div>
 					</div>
 				</div>
 
 				<div className="card">
-					<img src="./public/workscheduler.png" className="card-img" alt="note taker" />
+					<img src="./public/workscheduler.png" className="card-img" alt="scheduler" />
 					<div className="card-body">
 						<p className="cardText">A calendar app</p>
 						<div className="row justify-content-center">
 							<a href="https://jgalvez98.github.io/juanEG-work-day-scheduler/" className="cardBtn">Website</a>
-						</div>
+							<a href="https://github.com/jgalvez98/juanEG-work-day-scheduler" className="cardBtn2">Github</a>
+				</div>
 					</div>
 				</div>
 
@@ -71,11 +73,12 @@ export default function Cards() {
 						<p className="cardText">A note taking app</p>
 						<div className="row justify-content-center">
 							<a href="https://github.com/jgalvez98/JEG-NoteTaker" className="cardBtn">Website</a>
+							<a href="https://github.com/jgalvez98/JEG-NoteTaker" className="cardBtn2">Github</a>
+
 						</div>
 					</div>
 				</div>
-	
-			</div>
+	</div>
 		</div>
 	);
 };
--- src/components/UI/PortfolioContainer.jsx
+++ src/components/UI/PortfolioContainer.jsx
@@ -4,13 +4,9 @@ import Card from "../Card/Card";
 const PortfolioContainer = () => {
 
 	return (
-	  <>
-		  <section className="row d-flex justify-content-center">
-			<div className="col-sm-12 col-lg-8 d-flex justify-content-evenly flex-wrap custom-container-about-me">
-				<Card/>			
-			</div>
-		  </section>
-	  </>
+		<>
+			  <Card/>			
+	</>
 	);
   };
   
